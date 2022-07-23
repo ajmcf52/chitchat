b3dd1d4f9e24f59c84221d3ccb472485e6183ad5
ajmcf52 <andrew.mcfarlane52@gmail.com>
2022-07-23 15:47:36 -0700

    "Hello World!" complete. It compiles and runs.

---
+++ src/main/ChatterApp.java
@@ -0,0 +1,10 @@
+package main;
+/**
+ * main entry point for the chatter application.
+ */
+public class ChatterApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
