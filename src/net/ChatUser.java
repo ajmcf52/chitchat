c62ebb31ba0690a83d873d00bf7e77ba63030853
ajmcf52 <andrew.mcfarlane52@gmail.com>
2022-07-24 14:37:43 -0700

    created a base foundation of code for ChatUser; next to do is fleshing out run(), determining a good way of instantiating CU (which will be in the scope of ChatterApp) from the context of LoginPanel's KeyEventHandler method. Designing this instantiation cleanly will be a bit of a task, but it shouldn't be too bad.

---
+++ src/net/ChatUser.java
@@ -0,0 +1,60 @@
+package net;
+
+import java.lang.Thread;
+
+/**
+ * this class represents a thread-based ChatUser within the Chatter application.
+ */
+public class ChatUser extends Thread {
+    private String userID; //uniquely identifies this user.
+    private String alias;
+
+    /**
+     * default constructor.
+     */
+    public ChatUser() {
+        userID = "";
+        alias = "";
+    }
+
+    /**
+     * constructor that will more than likely be used most of the time.
+     * @param uid - user ID
+     * @param a - alias
+     */
+    public ChatUser(String uid, String a) {
+        userID = uid;
+        alias = a;
+    }
+
+    /**
+     * the ChatUser's main course of action.
+     */
+    public void run() {
+
+    }
+
+    /**
+     * getter for userID.
+     * @return userID
+     */
+    public String getUID() { return userID; }
+
+    /**
+     * setter for alias.
+     * @param a - ChatUser's new alias
+     */
+    public void setAlias(String a) {
+        if (a != null && a != "") {
+            alias = a;
+        }
+    }
+
+    /**
+     * getter for alias.
+     * @return ChatUser alias
+     */
+    public String getAlias() { return alias; }
+
+
+}
