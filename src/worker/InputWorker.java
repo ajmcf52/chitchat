4806214a7c00226c439d302ee66d0d7590e32ab5
ajmcf52 <andrew.mcfarlane52@gmail.com>
2022-08-06 16:50:18 -0700

    InputWorker OOP code cleanliness management for slightly different functionalities for User and SeshCoord.

--- src/worker/InputWorker.java
+++ src/worker/InputWorker.java
@@ -1,31 +1,25 @@
 package worker;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.concurrent.ArrayBlockingQueue;
-
-import misc.Constants;
-
-import java.net.Socket;
-import java.util.LinkedList;
 
 /**
  * this class is responsible for reading messages from a given Socket
- * and putting them into a message queue to be picked up by the SessionCoordinator.
- * Upon placing each message into the queue, the IW is then expected to notify the SC.
+ * and putting them into a message queue to be picked up by an entity on the other side.
+ * Each implementing subclass has its own way of notifying the correct entity on
+ * the other side of the message queue, which depends on context.
  */
-public class InputWorker extends Thread {
+public abstract class InputWorker extends Thread {
     
-    private BufferedReader in; // what will be used to read messages from.
-    private ArrayBlockingQueue<String> messageQueue; // where newly received messages will be placed.
+    protected BufferedReader in; // what will be used to read messages from.
+    protected ArrayBlockingQueue<String> messageQueue; // where newly received messages will be placed.
     /**
      * the flag below is used to signify whether this worker is running or not. Can be flipped off from outside
      * this worker's scope using the switchOff() method. From there, the worker thread dies and must be restarted.
      */
-     private volatile boolean isRunning;
-     private Object runLock; // lock used to check or switch the InputWorker's flag.
-     private Object newMessageNotifier; // notify this to let the SessionCoordinator know that new messages have come in.
-
+    protected volatile boolean isRunning;
+    protected Object runLock; // lock used to check or switch the InputWorker's flag.
+     
     /**
      * constructor of the InputWorker.
      * @param input what will be used to read incoming messages; initialized by SessionCoordinator.
@@ -33,54 +27,17 @@ public class InputWorker extends Thread {
      */
     public InputWorker(BufferedReader input, ArrayBlockingQueue<String> msgQueue) {
         runLock = new Object();
-        newMessageNotifier = new Object();
         isRunning = false;
         messageQueue = msgQueue;
         in = input;
     }
 
     /**
-     * this worker's main line of execution.
+     * used to signify that this thread worker has been turned on.
      */
-    public void run() {
-        // to temporarily hold messages.
-        // will only ever be accessed by this worker, therefore no synchronization needed.
-        LinkedList<String> messages = new LinkedList<String>(); 
-
+    public void switchOn() {
         synchronized (runLock) {
-            isRunning = true; // we will check this flag at the end of the while loop.
-        }
-        while (true) {
-
-            try {
-                /**
-                 * Block initially. We intentionally do this so while this thread has no input to read,
-                 * it doesn't "busy wait", allowing other threads to make use of available CPU cycles instead.
-                 */
-                String msg = in.readLine();
-                messages.add(msg);
-                // while there are messages to read, read them in one by one. ready() does not block.
-                while (in.ready()) {
-                    messages.add(in.readLine());
-                }
-                // add all received messages to the ABQ in the order by which they were received.
-                while (!messages.isEmpty()) {
-                    msg = messages.getFirst();
-                    messageQueue.put(msg);
-                }
-                // notify SessionCoordinator that new messages have come in.
-                newMessageNotifier.notify();
-
-            } catch (Exception e) {
-                System.out.println("InputWorker Error! --> " + e.getMessage());
-            }
-
-            // check to see if it is time to exit.
-            synchronized (runLock) {
-                if (!isRunning) {
-                    break;
-                }
-            }
+            isRunning = true;
         }
     }
 
